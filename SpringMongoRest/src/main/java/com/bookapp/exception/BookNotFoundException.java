3efd0a6b71c8ed7b53769cfe32fb5ad3003d2c79
99002758 <72496047+99002758@users.noreply.github.com>
2020-10-27 17:49:39 +0530

    Add files via upload

---
+++ SpringMongoRest/src/main/java/com/bookapp/exception/BookNotFoundException.java
@@ -0,0 +1,15 @@
+package com.bookapp.exception;
+
+public class BookNotFoundException extends RuntimeException {
+
+	public BookNotFoundException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public BookNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+}
